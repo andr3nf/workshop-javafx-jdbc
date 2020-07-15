73e438f3363162ff378e3e8b8f79276392660a33
andr3nf <nunesfigueiredo@gmail.com>
2020-07-15 11:51:27 -0300

    Criação da tela Sobre

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
 
@@ -28,12 +37,41 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	public void onMenuItemAboutAction() {
-		System.out.println("onMenuItemAboutAction");
+		loadView("/gui/About.fxml");  
 	}
 
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
 
 	}
+	
+	private synchronized void loadView(String absolutName) {
+		
+		try {
+			
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absolutName));
+			VBox newVBox = loader.load();
+			
+			// Pega a cena da tela principal
+			Scene mainScene = Main.getMainScene();
+			// Acessa o VBox da tela principal
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			// Pega o menu principal
+			Node mainMenu = mainVBox.getChildren().get(0);
+			// Limpa todo VBox
+			mainVBox.getChildren().clear();
+			// Adiciona o menu
+			mainVBox.getChildren().add(mainMenu);
+			// Adiciona o VBox da nova tela
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+		} catch (IOException e) {
+			
+			Alerts.showAlert("IOException", "Erro ao carregar a página", e.getMessage(), AlertType.ERROR);
+			
+		}
+	}
+	
 
 }
